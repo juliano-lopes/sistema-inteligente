f07c1e449c019da8b30cf6aecd2927e8f28ac558
Juliano Lopes <julopys@hotmail.com>
2024-09-29 05:52:50 -0300

    Code refactoring to define model and scaler path and load model in constructor.

--- api/model/model.py
+++ api/model/model.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0bfc5500ee79bf52d7e9c1661c2c9c9c5e9574928db539afdf01d3d01e21764e
-size 3269
+oid sha256:914237d2082bd94b35791fe202e017dbe12ffdb4c56c07f421656ce6d1cca9b0
+size 3402
