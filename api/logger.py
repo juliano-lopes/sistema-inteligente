2db2432d23996445821c811f577d71fe752564f1
juliano-lopes <julopys@hotmail.com>
2024-09-29 21:50:55 -0300

    Adds logger.

--- api/logger.py
+++ api/logger.py
@@ -1,3 +1,71 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:b5b053a2b8eafda842a3df4602c621cc8ff910ef9eaa36b7acd9c8ce59b81555
-size 2112
+from logging.config import dictConfig
+import logging
+import os
+
+
+log_path = "log/"
+# Verifica se o diretorio para armexanar os logs não existe
+if not os.path.exists(log_path):
+   # então cria o diretorio
+   os.makedirs(log_path)
+
+
+dictConfig({
+    "version": 1,
+    "disable_existing_loggers": True,
+    "formatters": {
+        "default": {
+            "format": "[%(asctime)s] %(levelname)-4s %(funcName)s() L%(lineno)-4d %(message)s",
+        },
+        "detailed": {
+            "format": "[%(asctime)s] %(levelname)-4s %(funcName)s() L%(lineno)-4d %(message)s - call_trace=%(pathname)s L%(lineno)-4d",
+        }
+    },
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+            "formatter": "default",
+            "stream": "ext://sys.stdout",
+        },
+        # "email": {
+        #     "class": "logging.handlers.SMTPHandler",
+        #     "formatter": "default",
+        #     "level": "ERROR",
+        #     "mailhost": ("smtp.example.com", 587),
+        #     "fromaddr": "devops@example.com",
+        #     "toaddrs": ["receiver@example.com", "receiver2@example.com"],
+        #     "subject": "Error Logs",
+        #     "credentials": ("username", "password"),
+        # },
+        "error_file": {
+            "class": "logging.handlers.RotatingFileHandler",
+            "formatter": "detailed",
+            "filename": "log/gunicorn.error.log",
+            "maxBytes": 10000,
+            "backupCount": 10,
+            "delay": "True",
+        },
+        "detailed_file": {
+            "class": "logging.handlers.RotatingFileHandler",
+            "formatter": "detailed",
+            "filename": "log/gunicorn.detailed.log",
+            "maxBytes": 10000,
+            "backupCount": 10,
+            "delay": "True",
+        }
+    },
+    "loggers": {
+        "gunicorn.error": {
+            "handlers": ["console", "error_file"],  #, email],
+            "level": "INFO",
+            "propagate": False,
+        }
+    },
+    "root": {
+        "handlers": ["console", "detailed_file"],
+        "level": "INFO",
+    }
+})
+
+
+logger = logging.getLogger(__name__)
